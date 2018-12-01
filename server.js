da2a2e551859461a0516af3838f6171427a5b6ac
Tyler G <guthriet1230@gmail.com>
2018-11-30 18:19:37 -0600

    front end complete

--- server.js
+++ server.js
@@ -28,3 +28,12 @@ app.get("/", function(req, res) {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+  // Find our form in the DOM using its class name.
+const form = document.getElementByClassName('.contact-form')[0];
+
+// Get the form data with our (yet to be defined) function.
+const data = getFormDataAsJSON(form);
+
+// Do something with the email address.
+//doSomething(data.email);
